f9afd8c642a6e5f7ddaa6795023b3a294c100c38
Holger <hadronx@live.com>
2025-05-18 00:45:29 +0300

    update functionality to load gpt-4o

--- config/ai_models.py
+++ config/ai_models.py
@@ -1,8 +1,19 @@
 import json
+import os
+from dotenv import load_dotenv
 
-def get_model(model_name):
+def get_model(model_name):    
     with open("config/models.json") as f:
         data = json.load(f)
+
+        if model_name == "OpenAI":
+            load_dotenv()   # load environment variables
+            api_key = os.getenv("OPENAI_CHAT_API_KEY")
+
+            if api_key is None:
+                raise ValueError("API key for OpenAI not found ...")
+            else:
+                data[model_name]["api_key"] = api_key
     
     return data[model_name]
 
@@ -16,4 +27,5 @@ def list_models():
 """
 if __name__ == "__main__":
     print(get_model("Deepseek"))
+    print(get_model("OpenAI"))
 """
